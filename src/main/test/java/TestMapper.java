eec742a9b67ac5b6c5e1dbc8ba7f171a8ccbf13d
wengzhongjie <1310120194@qq.com>
2018-11-27 09:31:17 +0800

    加了一句话

---
+++ src/main/test/java/TestMapper.java
@@ -0,0 +1,22 @@
+import com.tb121.ssm.entity.form.CourseRequest;
+import com.tb121.ssm.mapper.CourseMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMapper {
+    private CourseMapper courseMapper;
+    @Before
+    public void init() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+        courseMapper= (CourseMapper) ac.getBean("courseMapper");
+
+    }
+    @Test
+    public void fun() {
+        CourseRequest courseRequest=courseMapper.findById();
+        System.out.println(courseRequest);
+        System.out.println("随便加了的一句话");
+    }
+}
