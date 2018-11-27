55f5b511c2af848ae29244a615206d0ca8847a31
tchjava <43031306+tchjava@users.noreply.github.com>
2018-11-27 10:32:40 +0800

    Update TestEntityWrapper.java
    删除了fun5的内容

--- src/main/test/java/TestEntityWrapper.java
+++ src/main/test/java/TestEntityWrapper.java
@@ -81,9 +81,6 @@ public class TestEntityWrapper {
     }
     @Test
     public void fun5(){
-        //这里要补充
-        System.out.println("fun5...");
-        //xxx
-        System.out.print("这句话有错吗?");
+        
     }
 }
